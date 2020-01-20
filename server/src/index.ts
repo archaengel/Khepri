d6ffe1d69abe6d89c389c4e26778e6dc7d5eeaa2
Archaengel <god11341258@gmail.com>
2020-01-20 06:05:23 -0800

    Add server and Procfile

---
+++ server/src/index.ts
@@ -0,0 +1,12 @@
+import * as express from 'express';
+import { resolve } from 'path';
+
+const app = express();
+const port = 3000;
+
+app.use(express.static('../client/build'));
+app.get('*', (req, res) => {
+  res.sendFile(resolve(__dirname, '../client/build/index.html'));
+});
+
+app.listen(port, () => console.log(`[app]: Now listening on ${port}`));
